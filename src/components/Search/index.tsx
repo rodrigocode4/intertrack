1a4757be0bc29a97a635709c9322545b80951c9d
Rodrigo Oliveira <rodrigocode4@gmail.com>
2021-03-01 18:50:31 -0300

    add Button in CardCar component

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -3,12 +3,15 @@ import * as S from './styled'
 
 const Search = () => {
   return (
-    <S.SearchWrapper>
+    <S.Form>
       <S.SearchInput
         pattern="^[a-zA-Z]{3}-\d{4}$"
         placeholder="Pesquisar placa..."
       />
-    </S.SearchWrapper>
+      <S.SearchButton>
+        <S.SearchButtonIcon />
+      </S.SearchButton>
+    </S.Form>
   )
 }
 
--- src/components/Search/styled.tsx
+++ src/components/Search/styled.tsx
@@ -1,17 +1,24 @@
 import styled from 'styled-components'
+import { Search } from '@styled-icons/heroicons-outline/Search'
 
-export const SearchWrapper = styled.div``
-
-export const SearchInput = styled.input`
+export const Form = styled.form`
   width: 18.0625rem;
   height: 3.6875rem;
-  font-size: 1.375rem;
-  padding-left: 1.25rem;
   border-radius: 0.625rem;
-  background: var(--bg-secondary);
+  background-color: var(--bg-secondary);
+  box-shadow: 0rem 0.25rem 0.25rem rgba(0, 0, 0, 0.25);
+  display: flex;
+`
+
+export const SearchInput = styled.input`
+  width: 83%;
+  height: inherit;
+  border-radius: 0.625rem 0 0 0.625rem;
+  padding-left: 1.25rem;
+  font-size: 1.375rem;
+  background-color: transparent;
   border: none;
   font-family: Roboto;
-  box-shadow: 0rem 0.25rem 0.25rem rgba(0, 0, 0, 0.25);
   color: var(--text-primary);
   text-transform: uppercase;
 
@@ -21,5 +28,25 @@ export const SearchInput = styled.input`
     font-size: 1.375rem;
     color: var(--text-primary-placeholder);
     text-transform: lowercase;
+  }
+`
+
+export const SearchButtonIcon = styled(Search)`
+  color: var(--text-primary);
+  width: 1.7rem;
+  height: 1.7rem;
+`
+
+export const SearchButton = styled.button`
+  width: 17%;
+  height: inherit;
+  padding: 0;
+  border: 0;
+  background-color: transparent;
+  border-radius: 0 0.625rem 0.625rem 0;
+  border-left: 1px solid var(--text-primary-placeholder);
+
+  &:hover {
+    cursor: pointer;
   }
 `
