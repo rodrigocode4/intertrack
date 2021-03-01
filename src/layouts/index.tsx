98e1cb921f6f35b914018e53da9c8e7e195675e2
Rodrigo Oliveira <rodrigocode4@gmail.com>
2021-03-01 01:30:41 -0300

    add Home component

---
+++ src/layouts/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as S from './styled'
+import Home from 'pages/Home'
+
+const Layout = () => {
+  return (
+    <S.LayoutWaraper>
+      <Home />
+    </S.LayoutWaraper>
+  )
+}
+
+export default Layout
---
+++ src/layouts/styled.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const LayoutWaraper = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--bg-primary);
+`
