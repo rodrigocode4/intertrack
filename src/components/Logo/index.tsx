1c106da6a9e7fe30c2da1b629a5e78098c45ca1b
Rodrigo Oliveira <rodrigocode4@gmail.com>
2021-02-28 21:10:13 -0300

    fix

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import * as FullLogo from 'assets/full-logo.png'
+import FullLogo from 'assets/full-logo.png'
 import * as S from './styled'
 
 const Logo = () => {
   return (
-    <S.Logo src={FullLogo} alt="Imagem com logo da Intertrack Tecnologia" />
+    <S.Logo
+      src={FullLogo as string}
+      alt="Imagem com logo da Intertrack Tecnologia"
+    />
   )
 }
 
