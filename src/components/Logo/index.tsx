4e2ffa2373901c91f7e41788aa4d0009d985a19e
Rodrigo Oliveira <rodrigocode4@gmail.com>
2021-02-28 21:06:00 -0300

    add Logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as FullLogo from 'assets/full-logo.png'
+import * as S from './styled'
+
+const Logo = () => {
+  return (
+    <S.Logo src={FullLogo} alt="Imagem com logo da Intertrack Tecnologia" />
+  )
+}
+
+export default Logo
---
+++ src/components/Logo/styled.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Logo = styled.img`
+  width: 12.5rem;
+  height: 3.25rem;
+`
