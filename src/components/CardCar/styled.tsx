f72f7fba33f17a560e7f64ba165b6064b3a75a8f
Rodrigo Oliveira <rodrigocode4@gmail.com>
2021-03-02 16:36:42 -0300

    bug fix height

--- src/components/CardCar/styled.tsx
+++ src/components/CardCar/styled.tsx
@@ -2,25 +2,25 @@ import styled from 'styled-components'
 
 export const WrapperCardCar = styled.div`
   width: 18.0625rem;
-  height: 10.4375rem;
   background-color: var(--bg-secondary);
   box-shadow: 0 0.25rem 0.25rem rgba(0, 0, 0, 0.25);
   border-radius: 0.625rem;
-  padding: 1.25rem 2.0625rem 2.0625rem 2.25rem;
+  padding: 1.25rem 2.0625rem 1.0625rem 2.25rem;
   border-left: 0.5rem solid var(--bg-primary);
+  box-sizing: border-box;
 `
 
 export const CarBrand = styled.h1`
   color: var(--text-primary);
   font-family: Roboto;
-  font-weight: 500;
+  font-weight: 700;
   font-size: 1.375rem;
 `
 
 export const CarModel = styled.h2`
   color: var(--text-primary);
   font-family: Roboto;
-  font-weight: 500;
+  font-weight: 600;
   font-size: 1.15rem;
 `
 
