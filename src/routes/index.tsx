2732e6656d2fab9495d84abbf357d459b8fe1c4c
Rodrigo Oliveira <rodrigocode4@gmail.com>
2021-03-02 23:45:18 -0300

    add routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+
+import Home from 'pages/Home'
+import Vehicle from 'pages/Vehicle'
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/:id(\d+)">
+          <Vehicle />
+        </Route>
+        <Route>
+          <Redirect from="*" to="/" />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default Routes
